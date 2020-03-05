6fb87f56fa0948535ff19ba67a612037d0d4e595
acoyp99 <61330297+acoyp99@users.noreply.github.com>
2020-03-05 15:41:04 -0500

    Update ipsec.tf

--- ipsec.tf
+++ ipsec.tf
@@ -1,8 +1,4 @@
 
-resource "ibm_ipsec_vpn" "ipsec" {
+resource "ibm_ipsec_vpn" "ipsec1" {
     datacenter = "dal05"
-    Customer_Peer_IP = "192.168.57.123"
-    phase_one = [{Authentication="SHA1",Encryption="3DES",Diffie-Hellman-Group=12,Keylife=131}]
-    phase_two = [{Authentication="SHA1",Encryption="3DES",Diffie-Hellman-Group=12,Keylife=133}]
-    remote_subnet = [{Remote_ip_adress = "10.0.0.0",Remote_IP_CIDR = 22}]
     }
